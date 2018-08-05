e129407d5baed6f774b76300ca203cf3ad0ab3b7
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 17:31:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(spoken_string)
-  if spoken_string == spoken_string.downcase
+  if spoken_string.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif spoken_string == spoken_string.upcase
+  elsif spoken_string.upcase
     return "NO, NOT SINCE 1938!"
   elsif spoken_string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
