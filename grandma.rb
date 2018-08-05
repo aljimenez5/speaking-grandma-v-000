eb8eff3b33e620c6fb4876e0e402a2eef6bea342
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 17:22:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == !string.capitalized
+  if string == !string.capitalize
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.capitalized
+  elsif string == string.capitalize
     return "NO, NOT SINCE 1938!"
   else string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
