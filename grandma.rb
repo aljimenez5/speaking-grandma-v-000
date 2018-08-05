670848278bc3a7635d57d221b3ebb9d0fa62fd65
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-05 17:22:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.capitalized
     return "NO, NOT SINCE 1938!"
+  else string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
   
